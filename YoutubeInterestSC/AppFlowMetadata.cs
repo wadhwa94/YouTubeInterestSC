cc664fad6bad1dd82a3f553337b656538fb5a4a1
wadhwa94 <wadhwajatin94@gmail.com>
2018-04-11 15:24:07 +0530

    Add project files.

---
+++ YoutubeInterestSC/AppFlowMetadata.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System;
+using System.Web.Mvc;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.YouTube.v3;
+using Google.Apis.Util.Store;
+
+namespace YoutubeInterestSC
+{
+    public class AppFlowMetadata : FlowMetadata
+    {
+        private static readonly IAuthorizationCodeFlow flow =
+            new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = "1091996968324-gmpcveo6mc3ptgl4fmthr3n49tb73f81.apps.googleusercontent.com",
+                    ClientSecret = "vU6QcV020r9kUfgEYsXeZfJB"
+                },
+                Scopes = new[] { YouTubeService.Scope.YoutubeReadonly, YouTubeService.Scope.Youtube, YouTubeService.Scope.YoutubeForceSsl, YouTubeService.Scope.Youtubepartner, YouTubeService.Scope.YoutubeUpload },
+                DataStore = new FileDataStore("Drive.Api.Auth.Store")
+            });
+
+        public override string GetUserId(Controller controller)
+        {
+            // In this sample we use the session to store the user identifiers.
+            // That's not the best practice, because you should have a logic to identify
+            // a user. You might want to use "OpenID Connect".
+            // You can read more about the protocol in the following link:
+            // https://developers.google.com/accounts/docs/OAuth2Login.
+            var user = controller.Session["user"];
+            if (user == null)
+            {
+                user = Guid.NewGuid();
+                controller.Session["user"] = user;
+            }
+            return user.ToString();
+
+        }
+
+        public override IAuthorizationCodeFlow Flow
+        {
+            get { return flow; }
+        }
+    }
+}
---
+++ YoutubeInterestSC/Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.YouTube.v3;
+using Google.Apis.Services;
+
+namespace YoutubeInterestSC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<ActionResult> YouTubeInterest( int videoCount, CancellationToken cancellationToken)
+        {
+            var result = await new AuthorizationCodeMvcApp(this, new AppFlowMetadata()).
+                AuthorizeAsync(cancellationToken);
+
+            if (result.Credential != null)
+            {
+                var ytService = new YouTubeService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = result.Credential,
+                    ApplicationName = "ASP.NET MVC Sample"
+                });
+
+                // YOUR CODE SHOULD BE HERE..
+                // SAMPLE CODE:
+                //var playlistRequest = ytservice.Playlists.List("snippet");
+                //playlistRequest.Mine = true;
+                //var response = await playlistRequest.ExecuteAsync();
+
+                Dictionary<string, int> VidCategory_Array = new Dictionary<string, int>();
+                string message = "";
+                try
+                {
+                    var channelsRequest = ytService.Channels.List("contentDetails");
+                    channelsRequest.Mine = true;
+                    var likePlaylistIIdResponse = channelsRequest.Execute();
+                    Google.Apis.YouTube.v3.Data.PlaylistItemListResponse videosResponse = null;
+                    int currentVideoCount = 0;
+                    if (likePlaylistIIdResponse.Items.Count > 0)
+                    {
+                        string likePlayListId = likePlaylistIIdResponse.Items[0].ContentDetails.RelatedPlaylists.Likes;
+                        string nextPagetoken = null;
+                        do
+                        {
+                            var videosRequest = ytService.PlaylistItems.List("snippet");
+                            videosRequest.PlaylistId = likePlayListId;
+                            if (videoCount != -1)
+                                videosRequest.MaxResults = Math.Min(videoCount, 50);
+                            else
+                                videosRequest.MaxResults = 50;
+                            if (nextPagetoken != null)
+                            {
+                                if (videosResponse.NextPageToken != null)
+                                {
+                                    videosRequest.PageToken = videosResponse.NextPageToken;
+                                }
+                            }
+                            videosResponse = videosRequest.Execute();
+                            currentVideoCount += videosResponse.Items.Count;
+                            for (int i = 0; i < videosResponse.Items.Count; i++)
+                            {
+
+                                var videoId = videosResponse.Items[i].Snippet.ResourceId.VideoId; //itearate for all the videos
+
+                                var videoDetails = ytService.Videos.List("snippet");
+                                videoDetails.Id = videoId;
+                                var videoDetailsResponse = videoDetails.Execute();
+                                if (videoDetailsResponse.Items.Count > 0)
+                                {
+                                    var videoCategoryId = videoDetailsResponse.Items[0].Snippet.CategoryId;
+                                    var videoCategoryNameRequest = ytService.VideoCategories.List("snippet");
+                                    videoCategoryNameRequest.Id = videoCategoryId;
+                                    var videoCategoryNameResponse = videoCategoryNameRequest.Execute();
+                                    var videoCategoryName = videoCategoryNameResponse.Items[0].Snippet.Title;
+
+                                    if (VidCategory_Array.ContainsKey(videoCategoryName))
+                                    {
+                                        VidCategory_Array[videoCategoryName] += 1;
+                                    }
+                                    else
+                                    {
+                                        VidCategory_Array.Add(videoCategoryName, 1);
+                                    }
+                                }
+
+                            }
+
+                            if (videosResponse.NextPageToken != null)
+                                nextPagetoken = videosResponse.NextPageToken;
+                        } while (videosResponse.NextPageToken != null && videoCount == -1);
+                    }
+                    
+                    ViewBag.Message = "response IS: " + VidCategory_Array.ToString();
+                    //                    return VidCategory_Array;
+                }
+                catch (Exception e)
+                {
+                    ViewBag.Message = "response IS: " + VidCategory_Array.ToString();
+                }
+                return View();
+            }
+            //var list = await service.Files.List().ExecuteAsync();
+
+
+
+            else
+            {
+                return new RedirectResult(result.RedirectUri);
+            }
+        }
+    }
+}
